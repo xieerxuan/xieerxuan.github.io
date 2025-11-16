9be912f6e9cee282ff30b54e80b81fa9b247f79d
xieerxuan <2724235894@qq.com>
2025-11-16 18:21:39 +0800

    try correcting latex rendering problems

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1763279681',resources: [
+    cacheName: 'chirpy-1763287744',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
