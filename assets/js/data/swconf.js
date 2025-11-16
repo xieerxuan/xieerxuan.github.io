83e15875994fed0cdf22b9863d8cc65a1fc1c191
xieerxuan <2724235894@qq.com>
2025-11-16 17:55:28 +0800

    test workflow 2

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1759508918',resources: [
+    cacheName: 'chirpy-1763279681',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
