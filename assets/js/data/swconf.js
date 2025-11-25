94b3928a0e9d889bac435a9db718ff7a85f7b567
xieerxuan <2724235894@qq.com>
2025-11-25 14:29:28 +0800

    Add VAE

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1763289012',resources: [
+    cacheName: 'chirpy-1764051900',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
